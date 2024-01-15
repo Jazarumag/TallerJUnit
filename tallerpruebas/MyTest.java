07697c576501ef5a503e8eb384bfe9f32f080094
Jesus Suarez <jedasuar@espol.edu.ec>
2024-01-15 10:25:11 -0500

    Caso de prueba Seccion A

--- tallerpruebas/MyTest.java
+++ tallerpruebas/MyTest.java
@@ -9,5 +9,6 @@ public class MyTest {
 	 assertEquals(-1,Calculation.findMax(new int[]{-12,-1,-3,-4,-2}));
 	 assertEquals(99,Calculation.findMax(new int[] {99,-108,92,90,-1}));
 	 assertEquals(4,Calculation.findMax(new int[]{-12,1,0,4,1}));
+	 assertEquals(777,Calculation.findMax(new int[]{-214,123,352,777,-112}));
 	 }
 }
