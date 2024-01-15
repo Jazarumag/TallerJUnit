277ad5bca03958c3ee5006add1ea96e7924edbef
Jazarumag <joshzaruma@gmail.com>
2024-01-15 09:56:58 -0500

    Tal Cual

---
+++ tallerpruebas/MyTest.java
@@ -0,0 +1,10 @@
+package tallerpruebas;
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+public class MyTest {
+	@Test
+	 public static void testFindMax(){
+	 assertEquals(4,Calculation.findMax(new int[]{1,3,4,2}));
+	 assertEquals(-1,Calculation.findMax(new int[]{-12,-1,-3,-4,-2}));
+	 }
+}
