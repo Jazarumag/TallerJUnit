3088c05bbacf260c52c0c7810cf9f5a86a41c8c9
lborja04 <lborjaze@fiec.espol.edu.ec>
2024-01-15 10:58:43 -0500

    casos de prueba para Employee

--- tallerpruebas/MyTest.java
+++ tallerpruebas/MyTest.java
@@ -11,4 +11,34 @@ public class MyTest {
 	 assertEquals(4,Calculation.findMax(new int[]{-12,1,0,4,1}));
 	 assertEquals(777,Calculation.findMax(new int[]{-214,123,352,777,-112}));
 	 }
+	
+	@Test
+    public void testCsForWorkerWithUSD() {
+        Employee worker = new Employee(1000, "USD", 0.1F, EmployeeType.Worker);
+        float result = worker.cs();
+        assertEquals(1064.3, result, 0.1);
+    }
+
+	@Test
+    public void testCsForWorkerWithOtherCurrency() {
+        Employee worker = new Employee(1000, "EUR", 0.1F, EmployeeType.Worker);
+        float result = worker.cs();
+        assertEquals(1014.3, result, 0.1);
+    }
+
+    @Test
+    public void testCsForSupervisor() {
+        Employee supervisor = new Employee(1000, "USD", 0.1F, EmployeeType.Supervisor);
+        float result = supervisor.cs();
+        // Adjust this value based on your expected result for the Supervisor case
+        assertEquals(1064.3, result, 0.1);
+    }
+
+    @Test
+    public void testCsForManager() {
+        Employee manager = new Employee(1000, "USD", 0.1F, EmployeeType.Manager);
+        float result = manager.cs();
+        // Adjust this value based on your expected result for the Manager case
+        assertEquals(1064.4, result, 0.1);
+    }
 }
