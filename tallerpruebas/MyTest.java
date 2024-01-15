32de7dfce2edd9e3dee0a8495fbb6c77f449b8fb
lborja04 <lborjaze@fiec.espol.edu.ec>
2024-01-15 10:17:58 -0500

    primer caso de prueba

--- tallerpruebas/MyTest.java
+++ tallerpruebas/MyTest.java
@@ -1,10 +1,13 @@
 package tallerpruebas;
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
+
 public class MyTest {
 	@Test
-	 public static void testFindMax(){
+	 public void testFindMax(){
 	 assertEquals(4,Calculation.findMax(new int[]{1,3,4,2}));
 	 assertEquals(-1,Calculation.findMax(new int[]{-12,-1,-3,-4,-2}));
+	 
+	 assertEquals(99,Calculation.findMax(new int[] {99,-108,92,90,-1}));
 	 }
 }
