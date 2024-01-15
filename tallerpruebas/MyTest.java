04e7684bda9e2456bb018e0fe8bc5b473aeb531b
Jazarumag <joshzaruma@gmail.com>
2024-01-15 10:22:15 -0500

    Caso de prueba

--- tallerpruebas/MyTest.java
+++ tallerpruebas/MyTest.java
@@ -7,7 +7,7 @@ public class MyTest {
 	 public void testFindMax(){
 	 assertEquals(4,Calculation.findMax(new int[]{1,3,4,2}));
 	 assertEquals(-1,Calculation.findMax(new int[]{-12,-1,-3,-4,-2}));
-	 
 	 assertEquals(99,Calculation.findMax(new int[] {99,-108,92,90,-1}));
+	 assertEquals(4,Calculation.findMax(new int[]{-12,1,0,4,1}));
 	 }
 }
